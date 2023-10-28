e4fffab598f0c5816b0bf2e8e223524198e406b7
Hiddenfeet <134339758+Hiddenfeet@users.noreply.github.com>
2023-10-29 01:00:41 +0200

    1

--- verify.mjs
+++ verify.mjs
@@ -1,10 +1,12 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { CronosBeta } from "@thirdweb-dev/chains" // Chain the contract you want to verify is on
 
-const sdk = new ThirdwebSDK(CronosBeta)
+const sdk = new ThirdwebSDK(CronosBeta, {
+  clientId: "21f95fcb128728a6cc6ae3cc3fab58a594e51bab949036b4a5cfaa2297d3aea84a520e0e850eef6eca10f7a5d3f091daa73425fa7932ebe5e5eb101727d261b7",
+});
 const explorerAPIUrl = "https://api.cronoscan.com/api"; // e.g. https://api.etherscan.io/api
 const explorerAPIKey = "BBSCND1TXVSE9WRVA6QGKH1CJ283VX5QA7"; // Generate API key on the explorer
-const contractAddress = "0xbADf379c542c30CC55FA5b0047Db5de752C38160"
+const contractAddress = "0xbADf379c542c30CC55FA5b0047Db5de752C38160";
 await sdk.verifier.verifyThirdwebContract(
   "DropERC721",
   // "DropERC1155",
